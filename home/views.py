8550e20f3e783ada306265c7d2743dc90447e922
Saurav <sp.gharti@gmail.com>
2018-07-10 00:15:01 +0000

    add package django-analytical

--- home/views.py
+++ home/views.py
@@ -7,6 +7,7 @@ from home.models import CustomText, HomePage
 
 def home(request):
     packages = [
+	{'name':'django-analytical', 'url': 'http://pypi.python.org/pypi/django-analytical/2.4.0'},
     ]
     context = {
         'customtext': CustomText.objects.first(),
